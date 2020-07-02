2893fed99f1cffe4c7d05c7bd6865e7daab1d0ae
Valentoine <vava78@live.fr>
2020-07-02 14:58:08 +0000

    Add title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,32 +3,26 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   def setup
-    @lavariable = "J'aime les chattes"
+    @lavariable = "J'aime les chats"
   end
   
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    #assert_select "title", "Home | #{@lavariable}"
+    assert_select "title", "Sample App"
 
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    #assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | Sample App"
   end
   
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    #assert_select "title", "About | Ruby on Rails Tutorial Sample App"
-  end
-  
-  test "should get contact" do
-    get static_pages_contact_url
-    assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | Sample App"
   end
   
   test "should get root" do
