3b125aaa9f681fa705a26a3f9ad0a840c8100c5b
Valentoine <vava78@live.fr>
2020-07-07 14:17:48 +0000

    Users REST finish

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,11 @@ module SessionsHelper
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
+  
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user && user == current_user
+  end
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
@@ -20,5 +25,10 @@ module SessionsHelper
   def log_out
     reset_session
     @current_user = nil
+  end
+  
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
