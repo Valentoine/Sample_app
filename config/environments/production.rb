94ae653f8cdc0843166327a9dd8f2dd6750e2cab
Valentoine <vava78@live.fr>
2020-07-08 10:08:33 +0000

    Change env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,10 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://drawbotics-tuto.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.perform_deliveries = true
+  
+  #host = 'drawbotics-tuto.herokuapp.com'
+  
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
