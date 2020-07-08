6c9b115758bb7e71c33db5916a3beb703dfb0fb3
Valentoine <vava78@live.fr>
2020-07-08 10:13:35 +0000

    Change env file 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   
-  #host = 'drawbotics-tuto.herokuapp.com'
+  host = 'drawbotics-tuto.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
